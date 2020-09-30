c995397cf1c61faf1a879530ac58221f93f41d4b
zhangyi <zhangyi@mediinfo.com.cn>
2020-09-30 11:08:19 +0800

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace yDesign
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
